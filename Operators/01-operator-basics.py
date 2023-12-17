b23075641ff2bad1b3e4b4c047ae2175bbd44428
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-12-17 19:59:22 +0530

    basics of operator

---
+++ Operators/01-operator-basics.py
@@ -0,0 +1,117 @@
+# Python Operators
+
+# Python Operators
+# Operators are used to perform operations on variables and values.
+# e.g 
+# + operator use for concat string and also use for addinf two value
+# print("Hello" + "World")
+# print(10+5)
+
+# Python divides the operators in the following groups:
+
+# 1. Arithmetic operators (+, -, *, /, %, **, //)
+
+# e.g
+
+value_of_a = 77
+value_of_b = 63
+
+# addition
+print("Sum: ", value_of_a + value_of_b )
+
+# Subtraction
+print("Subtraction", value_of_a - value_of_b)
+
+# multiplication
+print("Multiplication: ", value_of_a * value_of_b)
+
+# division
+print("Division", value_of_a/value_of_b)
+
+# floor division
+print("Floor division: ", value_of_a // value_of_b)
+
+# Modulo
+print("Modulo: " ,value_of_a % value_of_b)
+
+# a to the power b
+print("Power: " , value_of_a ** value_of_b)
+
+# 2. Assignment operators (=, +=, -=, *= , /= , %=, **=, //=, &=, !=, ^=, >>=, <<=)
+
+# e.g
+# assign 10 to var a
+var_a = 10
+
+# assign 5 to var b
+var_b = 5
+
+# assign the sum of a and b to a
+
+# var_a = var_a + var_b
+
+var_a += var_b
+
+print(var_a)
+
+# 3. Comparison operators(==, !=, >, <, >=, <=)
+# e.g
+
+a = 5
+
+b = 2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
+
+
+# 4. Logical operators(and, or ,not)
+# e.g
+
+a = 5
+b = 6
+
+print(( a > 2 ) and (b >=6))
+
+# Logical AND
+print(True and True)
+print(True and False)
+
+# Logical OR
+print(True or False)
+
+# Logical NOT
+print(not True)
+# 
+# 5. Identify operators(is, is not)
+
+# 6. Membership operators(in, not in)
+
+# 7. Bitwise operators
+      # a. & - AND
+      # b. | - OR
+      # c. ^ - XOR
+      # d. ~ - NOT
+      # e. << - Zero fill left shift
+      # f. >> - Signed right shift
+
+# Operator precedence
+# Operator precedence describes the order in which operations are performed.
+
+# ** Remember BODMOS concept.
+
